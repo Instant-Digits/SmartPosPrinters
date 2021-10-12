7fcd4f26417d7104d4d72022a37800a362842d71
shagananthiru <shaganan@gmail.com>
2021-10-12 11:02:01 +0530

    printers adding

--- Souce Python/DotMatrixPrinter.py
+++ Souce Python/DotMatrixPrinter.py
@@ -14,15 +14,33 @@ printerData= db.child('test1/Printer').get().val();
 printerData= dict(printerData);
 printingHeader=printerData['PrintingDataHeader'];
 
-printData= db.child('test1/print').get().val();
+#printData= db.child('test1/thermalPrint').get().val();
 
-for (key, value) in printData['itemList'].items():
-    print(value['make']+' '+value['item'] );
-    print ("{:<3} {:<15} {:<10} ".format(' ', 
-            str(value['quantity']) +' x Rs.'+currencyFormater(value['unitPrice']), 
-            'Rs.'+currencyFormater(value['unitPrice']*value['quantity'])
-            ));
+def PrinterJob(printData):
+    for (key, value) in printData['itemList'].items():
+        print(value['make']+' '+value['item'] );
+        print ("{:<1} {:<15} {right_aligned:>12} ".format(' ', 
+                str(value['quantity']) +' x Rs.'+currencyFormater(value['unitPrice']), 
+                right_aligned='Rs.'+currencyFormater(value['unitPrice']*value['quantity'])
+                ));
+    print ('')
+    print ("{:<1} {:<14} {:<10}".format('','TOTAL','Rs. '+currencyFormater(printData['total'])))
 
-print ('')
-print ("{:<2} {:<14} {:<10}".format('','TOTAL','Rs. '+currencyFormater(printData['total'])))
+def listener(message):
+	data=message["data"]
+	if(data):
+		db.child('test1/thermalPrint').remove()
+		PrinterJob(data)
+		
+
+db.child('test1/thermalPrint').stream(listener)
+
+
+# left_aligned = "Left Align"
+# center = "Centered"
+# right_aligned = "Right Align"
+# print("{left_aligned:<15}{center:^10}{right_aligned:>15}".format(
+# left_aligned=left_aligned,
+# center=center,
+# right_aligned=right_aligned))
 
